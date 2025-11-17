5c8868eb5fe8e5ecab4485b0fbeb4db8de7eb013
Mohd Faizan <umohdfaizan@gmail.com>
2025-11-18 00:26:07 +0530

    Update feedbackModel.js

--- Backend/models/feedbackModel.js
+++ Backend/models/feedbackModel.js
@@ -1,67 +1,41 @@
+
+
+
+
+
+
+
 const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+        ref: 'Student',
+        required: true
+    },
+    quizId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Quiz',
+        required: true
     },
     message: {
         type: String,
         required: true,
-        maxlength: 1000,
+        maxlength: 1000
     },
     rating: {
         type: Number,
         required: true,
         min: 1,
-        max: 5,
+        max: 5
     },
     createdAt: {
         type: Date,
-        default: Date.now,
+        default: Date.now
     }
 });
 
-
-
-
-
-
-
-
-
-// const mongoose = require('mongoose');
-
-// const feedbackSchema = new mongoose.Schema({
-//     userId: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'Student',
-//         required: true
-//     },
-//     quizId: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'Quiz',
-//         required: true
-//     },
-//     message: {
-//         type: String,
-//         required: true,
-//         maxlength: 1000
-//     },
-//     rating: {
-//         type: Number,
-//         required: true,
-//         min: 1,
-//         max: 5
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: Date.now
-//     }
-// });
-
-// module.exports = mongoose.model("Feedback", feedbackSchema);
+module.exports = mongoose.model("Feedback", feedbackSchema);
 
 
 
