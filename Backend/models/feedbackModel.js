9e6dcbb6b722f50a1bdfaaa6859c55fa09964a64
Mohd Faizan <umohdfaizan@gmail.com>
2025-11-17 18:01:48 +0530

    Update feedbackModel.js

--- Backend/models/feedbackModel.js
+++ Backend/models/feedbackModel.js
@@ -22,3 +22,61 @@ const feedbackSchema = new mongoose.Schema({
         default: Date.now,
     }
 });
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const feedbackSchema = new mongoose.Schema({
+//     userId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Student',
+//         required: true
+//     },
+//     quizId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Quiz',
+//         required: true
+//     },
+//     message: {
+//         type: String,
+//         required: true,
+//         maxlength: 1000
+//     },
+//     rating: {
+//         type: Number,
+//         required: true,
+//         min: 1,
+//         max: 5
+//     },
+//     createdAt: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
+
+// module.exports = mongoose.model("Feedback", feedbackSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
