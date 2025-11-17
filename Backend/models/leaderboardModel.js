e95149e041e8ade95a94d4393ef91d472b24505c
Mohd Faizan <umohdfaizan@gmail.com>
2025-11-18 00:30:42 +0530

    Update leaderboardModel.js

--- Backend/models/leaderboardModel.js
+++ Backend/models/leaderboardModel.js
@@ -8,7 +8,7 @@ const leaderboardSchema = new mongoose.Schema({
     },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'students',
         required: true,
     },
     score: {
