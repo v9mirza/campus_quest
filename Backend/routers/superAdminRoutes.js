f5cc8a95f52f13c73ad64999aea80c7e915f2e22
Mohd Faizan <umohdfaizan@gmail.com>
2025-11-22 17:22:22 +0530

    Update superAdminRoutes.js

--- Backend/routers/superAdminRoutes.js
+++ Backend/routers/superAdminRoutes.js
@@ -1,1 +1,17 @@
 
+const express = require("express");
+const router = express.Router();
+
+const {
+  registerSuperAdmin,
+  loginSuperAdmin
+} = require("../controllers/superAdminController");
+
+
+// ✔ Register HOD / Super Admin
+router.post("/register", registerSuperAdmin);
+
+// ✔ Login Super Admin
+router.post("/login", loginSuperAdmin);
+
+module.exports = router;
