ab7b8ba5579005f3633a11a1a772a2b481a3fd8b
v9mirza <v9mirza@gmail.com>
2025-12-23 12:56:32 +0000

    something something

--- Frontend/my-app/src/Pages.jsx
+++ Frontend/my-app/src/Pages.jsx
@@ -1,28 +1,21 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import CreateQuiz from './pages/CreateQuiz';
-<<<<<<< HEAD
 import QuestionsPage from './pages/QuestionsPage';
-=======
-import Login from './pages/Login';
-import Signup from './pages/Signup';
->>>>>>> df18ba1920e9fab43f2fe66e6b588cd5e1350568
+import Login from './pages/studentDetails/Login';
+import Signup from './pages/studentDetails/Signup';
 
 const Pages = () => {
   return (
     <div>
       <Routes>
         <Route path="/create-quiz" element={<CreateQuiz />} />
-<<<<<<< HEAD
-        <Route path="/questions" element={<QuestionsPage/>}/>
-        </Routes>
-=======
+        <Route path="/questions" element={<QuestionsPage />} />
         <Route path="/student/login" element={<Login />} />
         <Route path="/student/signup" element={<Signup />} />
       </Routes>
->>>>>>> df18ba1920e9fab43f2fe66e6b588cd5e1350568
     </div>
   )
 }
 
-export default Pages
+export default Pages;
